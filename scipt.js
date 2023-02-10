d5a717ba490ee0e1000433a52c015ac452ba171e
samitkapoor <samitkapoor77@gmail.com>
2023-02-11 00:48:42 +0530

    Fixed HTML and added link switch feature

--- scipt.js
+++ scipt.js
@@ -1,13 +1,19 @@
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(idName, linkId) {
+  var tablinks = document.getElementsByClassName("tab-links");
+  var tabcontents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for( tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    addEventListener.currentTarget.classList.add("active-link");
-   document.getElementById(tabname).classList.add("active-tab");
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+
+  //   addEventListener.currentTarget.classList.add("active-link");
+  //   document.getElementById(idName).classList.add("active-tab");
+
+  var tabContent = document.getElementById(idName);
+  tabContent.classList.add("active-tab");
+  var tabLink = document.getElementById(linkId);
+  tabLink.classList.add("active-link");
 }
